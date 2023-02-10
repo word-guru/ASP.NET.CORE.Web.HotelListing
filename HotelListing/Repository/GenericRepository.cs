289d2c934b0a0ffabb7030ccc619600495c6d137
Pavel P <legeon48@mail.ru>
2023-02-10 17:14:57 +0300

    Add GenericRepository

---
+++ HotelListing/Repository/GenericRepository.cs
@@ -0,0 +1,65 @@
+﻿using HotelListing.Data.DBContext;
+using HotelListing.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(AppDbContext context)
+        {
+            _context = context;
+            _db = _context.Set<T>();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await _db.FindAsync(id);
+            if (entity == null)
+            {
+                throw new ArgumentException(); //Заменить
+            }
+            else
+            {
+                _db.Remove(entity);
+            }
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
