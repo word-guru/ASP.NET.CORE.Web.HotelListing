55408b299d280597c1d60964dc06b815f1dc573e
Pavel P <legeon48@mail.ru>
2023-02-10 16:07:17 +0300

    Update db

--- HotelListing/Data/DBContext/AppDbContext.cs
+++ HotelListing/Data/DBContext/AppDbContext.cs
@@ -9,6 +9,40 @@ namespace HotelListing.Data.DBContext
         
         public DbSet<Country> Countrys { get; set; }
         public DbSet<Hotel> Hotels { get; set; }
-        
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country { Id = 1, Name = "Jamaica", ShortName = "JM" },
+                new Country { Id = 2, Name = "Bahamas", ShortName = "BS" },
+                new Country { Id = 3, Name = "Cayman Island", ShortName = "CI" }
+                );
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort And Spa",
+                    Addres = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Addres = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Addres = "Nassua",
+                    CountryId = 2,
+                    Rating = 4.8
+                }
+                );
+        }
     }
 }
--- HotelListing/Migrations/20230210130456_SeedingData.Designer.cs
+++ HotelListing/Migrations/20230210130456_SeedingData.Designer.cs
@@ -3,14 +3,16 @@ using HotelListing.Data.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelListing.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230210130456_SeedingData")]
+    partial class SeedingData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
